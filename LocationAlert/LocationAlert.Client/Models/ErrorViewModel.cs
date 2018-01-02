2c92c2eff19ae4a29cbe274ad2324b9ac347b0b8
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-02 13:41:43 -0500

    Initial Commit of Client Project

---
+++ LocationAlert/LocationAlert.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LocationAlert.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
