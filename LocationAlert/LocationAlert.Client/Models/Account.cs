211fd28b633d179e578f057faef54bb9b3c5fb80
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-02 14:33:15 -0500

    Add Account Model

---
+++ LocationAlert/LocationAlert.Client/Models/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LocationAlert.Client.Models
+{
+    public class Account
+    {
+        public MailAddress Email { get; set; }
+
+        public String Password { get; set; }
+
+        public String FirstName { get; set; }
+        public String MiddleInitial { get; set; }
+        public String LastName { get; set; }
+
+        public String Phone { get; set; }
+
+
+        public Account()
+        {
+            Email = new MailAddress("undefined");
+        }
+
+    }
+}
