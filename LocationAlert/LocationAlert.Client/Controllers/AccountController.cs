76d1047e2cfd34d001567fabc7484743e00c5192
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-03 08:53:58 -0500

    Add Session extension class (not tested yet)

--- LocationAlert/LocationAlert.Client/Controllers/AccountController.cs
+++ LocationAlert/LocationAlert.Client/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Session;
 
 namespace LocationAlert.Client.Controllers
 {
@@ -12,6 +13,7 @@ namespace LocationAlert.Client.Controllers
         public IActionResult Index()
         {
             HttpContext.Session.SetInt32("SessionKey", 5);
+
             return View();
 
         }
---
+++ LocationAlert/LocationAlert.Client/Models/SessionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationAlert.Client.Models
+{
+
+    // Copied From : https://docs.microsoft.com/en-us/aspnet/core/fundamentals/app-state?tabs=aspnetcore2x
+    // Allows us to serialize the Account object to save in Session
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) :
+                                  JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
