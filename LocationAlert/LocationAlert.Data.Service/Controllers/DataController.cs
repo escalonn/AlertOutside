1863230321a47bccd299a90282d1a4e70dc7e95f
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-17 14:13:38 -0500

    Scaffold Datacontroller

---
+++ LocationAlert/LocationAlert.Data.Service/Controllers/DataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationAlert.Data.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Data")]
+    public class DataController : Controller
+    {
+        // POST data/register
+        [HttpPost]
+        public IActionResult Register()
+        {
+            // validate and talk to database
+            return Ok();
+        }
+
+        // POST data/login
+        [HttpPost]
+        public IActionResult Login()
+        {
+            // validate and talk to database
+            return Ok();
+        }
+
+        // POST data/update
+        [HttpPost]
+        public IActionResult Update()
+        {
+            // validate and talk to database
+            return Ok();
+        }
+    }
+}
