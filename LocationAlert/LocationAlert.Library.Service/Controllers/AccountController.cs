d1d5956c8b236eab3096ea9b1654da29d1b3227b
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-17 10:11:10 -0500

    Scaffold Account controller

---
+++ LocationAlert/LocationAlert.Library.Service/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationAlert.Library.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+        // POST account/register
+        [HttpPost]
+        public void Register([FromBody] Account client)
+        {
+            return client;
+        }
+
+        // POST account/login
+        [HttpPost]
+        public void Login([FromBody] Account client)
+        {
+
+        }
+
+        // POST account/update
+        [HttpPost]
+        public void Update([FromBody] Account client)
+        {
+
+        }
+
+    }
+}
