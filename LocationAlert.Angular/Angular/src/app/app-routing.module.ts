797ab08eba22cd7e70f8c1b132e280694030382b
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-18 22:04:10 -0500

    Added routing for home and preference components

--- LocationAlert.Angular/Angular/src/app/app-routing.module.ts
+++ LocationAlert.Angular/Angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PreferencesComponent } from './preferences/preferences.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'preferences', component: PreferencesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
