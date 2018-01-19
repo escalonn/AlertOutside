8535c19eecd593d532123f826454349fc3ccd718
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-19 10:08:09 -0500

    Add proper time interval selectors

--- LocationAlert.Angular/Angular/src/app/weatherpreferences/weatherpreferences.component.ts
+++ LocationAlert.Angular/Angular/src/app/weatherpreferences/weatherpreferences.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-weatherpreferences',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherpreferencesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  onSave(){
+    // Send ouur data!
+  }
+
+  onBack(){
+    this.router.navigate(['preferences']);
+  }
 
   ngOnInit() {
   }
