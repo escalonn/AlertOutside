58360ef07aa70d4751f0b21d04a872716b30471e
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-01-19 15:33:12 -0500

    finish removing hard references within solution where service should do job.

--- LocationAlert/LocationAlert.Library/Models/LibHelper.cs
+++ LocationAlert/LocationAlert.Library/Models/LibHelper.cs
@@ -1,6 +1,6 @@
-﻿using LocationAlert.Data;
-using LocationAlert.Data.Models;
-using Dac = LocationAlert.Data.Models;
+﻿//using LocationAlert.Data;
+//using LocationAlert.Data.Models;
+//using Dac = LocationAlert.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,133 +11,133 @@ namespace LocationAlert.Library.Models
 {
     public class LibHelper
     {
-        public bool LoggedIn = false;
-        private static LibHelper _LH;
-        private static LocationAlertDBContext _DB = new LocationAlertDBContext();
-        private LibHelper(){}
+        //public bool LoggedIn = false;
+        //private static LibHelper _LH;
+        ////private static LocationAlertDBContext _DB = new LocationAlertDBContext();
+        //private LibHelper(){}
 
-        public static LibHelper Instance()
-        {
-            if (_LH == null)
-            {
-                _LH = new LibHelper();
-            }
+        //public static LibHelper Instance()
+        //{
+        //    if (_LH == null)
+        //    {
+        //        _LH = new LibHelper();
+        //    }
 
-            return _LH;
+        //    return _LH;
 
-        }
+        //}
 
-        public bool AddClient(Register r)
-        {
-            try
-            {
-                if (r == null)
-                {
-                    return false;
-                }
+        //public bool AddClient(Register r)
+        //{
+        //    try
+        //    {
+        //        if (r == null)
+        //        {
+        //            return false;
+        //        }
 
-                var ps = ClientToData(new List<Register>() { r }).FirstOrDefault();
+        //        var ps = ClientToData(new List<Register>() { r }).FirstOrDefault();
               
-                _DB.Client.Add(ps);
-                _DB.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
+        //        //_DB.Client.Add(ps);
+        //        //_DB.SaveChanges();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return false;
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
 
-        private List<Dac.Client> ClientToData(List<Register> ps)
-        {
-            var ls = new List<Dac.Client>();
+        //private List<Dac.Client> ClientToData(List<Register> ps)
+        //{
+        //    var ls = new List<Dac.Client>();
 
-            foreach (var item in ps)
-            {
-                ls.Add(new Dac.Client()
-                {
-                    FirstName = item.Firstname,
-                    Email = item.Email,
-                    PasswordHash = item.Password,
-                    DateModified = DateTime.Now,
+        //    foreach (var item in ps)
+        //    {
+        //        ls.Add(new Dac.Client()
+        //        {
+        //            FirstName = item.Firstname,
+        //            Email = item.Email,
+        //            PasswordHash = item.Password,
+        //            DateModified = DateTime.Now,
                    
-                });
-            }
+        //        });
+        //    }
 
-            return ls;
-        }
+        //    return ls;
+        //}
 
-        //Library to data--------------------
-        public bool AddRegion(Region r)
-        {
-            try
-            {
-                if (r == null)
-                {
-                    return false;
-                }
+        ////Library to data--------------------
+        //public bool AddRegion(Region r)
+        //{
+        //    try
+        //    {
+        //        if (r == null)
+        //        {
+        //            return false;
+        //        }
 
-                var ps = RegionToData(new List<Region>() { r }).FirstOrDefault();
+        //        var ps = RegionToData(new List<Region>() { r }).FirstOrDefault();
 
-                _DB.Region.Add(ps);
-                _DB.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
+        //        _DB.Region.Add(ps);
+        //        _DB.SaveChanges();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return false;
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
 
-        private List<Dac.Region> RegionToData(List<Region> ps)
-        {
-            var ls = new List<Dac.Region>();
+        //private List<Dac.Region> RegionToData(List<Region> ps)
+        //{
+        //    var ls = new List<Dac.Region>();
 
-            foreach (var item in ps)
-            {
-                ls.Add(new Dac.Region()
-                {
-                    Longitude = item.Longitude,
-                    Latitude = item.Latitude,
-                    Radius = item.Radius,
+        //    foreach (var item in ps)
+        //    {
+        //        ls.Add(new Dac.Region()
+        //        {
+        //            Longitude = item.Longitude,
+        //            Latitude = item.Latitude,
+        //            Radius = item.Radius,
                   
 
-                });
-            }
+        //        });
+        //    }
 
-            return ls;
-        }
+        //    return ls;
+        //}
 
-        //--------------------------------------------------------------------------
-        public bool LogIn(string firstname, string password)
-        {
-            if (_DB.Client.Any(acc => acc.FirstName == firstname && acc.PasswordHash == password))
-            {
-                LoggedIn = true;
-                return true;
-            }
-            return false;
-        }
+        ////--------------------------------------------------------------------------
+        //public bool LogIn(string firstname, string password)
+        //{
+        //    if (_DB.Client.Any(acc => acc.FirstName == firstname && acc.PasswordHash == password))
+        //    {
+        //        LoggedIn = true;
+        //        return true;
+        //    }
+        //    return false;
+        //}
 
-        public Account GetAccount(string firstName)
-        {
-            var dba = (from acc in _DB.Client
-                       where acc.FirstName == firstName
-                       select acc).First();
+        //public Account GetAccount(string firstName)
+        //{
+        //    var dba = (from acc in _DB.Client
+        //               where acc.FirstName == firstName
+        //               select acc).First();
 
-            return AccountToLibrary(dba);
-        }
+        //    return AccountToLibrary(dba);
+        //}
 
-        public Account AccountToLibrary(Dac.Client dba)
-        {
-            var la = new Account()
-            {
-                FirstName = dba.FirstName,
-                Password = dba.PasswordHash
-            };
-            return la;
-        }
+        //public Account AccountToLibrary(Dac.Client dba)
+        //{
+        //    var la = new Account()
+        //    {
+        //        FirstName = dba.FirstName,
+        //        Password = dba.PasswordHash
+        //    };
+        //    return la;
+        //}
     }
 }
