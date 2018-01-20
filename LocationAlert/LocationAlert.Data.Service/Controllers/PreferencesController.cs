a15a509d550c96681ee3f56ccc33cc16de6c54a8
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-01-20 12:08:35 -0500

    make data service endpoints more resty.

---
+++ LocationAlert/LocationAlert.Data.Service/Controllers/PreferencesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocationAlert.Data.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class PreferencesController : Controller
+    {
+        // GET preferences/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // return preferences of all accounts to caller
+            return Ok();
+        }
+
+        // GET preferences/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // return preferences of an account to caller
+            // (for when a user is still logged in but the client doesn't know his prefs anymore)
+            return Ok();
+        }
+    }
+}
