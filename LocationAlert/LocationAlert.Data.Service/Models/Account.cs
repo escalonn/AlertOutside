408e8a31cccdf6fdf81e7c81183506527c9bbfc4
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-20 17:06:33 -0500

    Add binding models

---
+++ LocationAlert/LocationAlert.Data.Service/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationAlert.Data.Service.Models
+{
+    public class Account
+    {
+        public string Email { get; set; }
+
+        public List<Region> Regions { get; set; } = new List<Region>();
+
+        public WeatherPreference WeatherPref { get; set; } = new WeatherPreference();
+    }
+}
---
+++ LocationAlert/LocationAlert.Data.Service/Models/Region.cs
@@ -0,0 +1,13 @@
+﻿namespace LocationAlert.Data.Service.Models
+{
+    public class Region
+    {
+        public int id { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public long Radius { get; set; }
+    }
+}
