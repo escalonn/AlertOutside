4fa1582e47096ed7bf5ea29ce3b45d081f479fa0
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-21 14:19:16 -0500

    Put weatherAPI call code into seperate class

---
+++ LocationAlert/LocationAlert.Library/Models/WeatherApi.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
+
+namespace LocationAlert.Library.Models
+{
+    // Used to make API call, and to store returned data
+    public class WeatherApi
+    {
+        // see api at http://openweathermap.org/current
+        internal class WeatherResponse
+        {
+            [JsonProperty("main")]
+            internal WeatherResponseMain Main { get; set; }
+        }
+
+        internal class WeatherResponseMain
+        {
+            [JsonProperty("temp")]
+            internal double Temperature { get; set; }
+        }
+
+        private static string OpenWeatherMapApiKey = "f44745b3b949068be733eb938051eed4";
+
+        public decimal Longitude { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public float TempInFah { get; set; }
+
+        //public WeatherPreference() { }
+
+        //public WeatherPreference(decimal latitude, decimal longitude)
+        //{
+        //    Latitude = latitude;
+        //    Longitude = longitude;
+        //}
+
+        // do not run more than once per 10 minutes!
+        // see: https://www.openweathermap.org/appid
+        public async Task<double> GetWeatherForecastAsync(double latitude, double longitude)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var baseApiUri = "http://api.openweathermap.org/data/2.5/weather";
+                    var queryString = new Dictionary<string, string>()
+                    {
+                        ["lat"] = latitude.ToString(),
+                        ["long"] = longitude.ToString(),
+                        ["APPID"] = OpenWeatherMapApiKey
+                    };
+                    string fullApiUri = QueryHelpers.AddQueryString(baseApiUri, queryString);
+                    HttpResponseMessage response = await client.GetAsync(fullApiUri);
+                    response.EnsureSuccessStatusCode();
+
+                    string stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<WeatherResponse>(stringResult);
+                    double temperatureK = rawWeather.Main.Temperature;
+                    double temperatureF = KelvinToFahrenheit(temperatureK);
+                    return temperatureF;
+                }
+                catch (HttpRequestException)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static double KelvinToFahrenheit(double tempKelvin)
+        {
+            return tempKelvin * 9 / 5 - 459.67;
+        }
+    }
+}
