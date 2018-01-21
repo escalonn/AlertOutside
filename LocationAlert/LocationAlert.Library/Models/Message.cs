2e5324df67eca52d93c978ed3d46823a6f88139c
Brandon Frankenfield <bran.frankenfield@gmail.com>
2018-01-21 13:54:35 -0500

    Add all preferences to compose message

--- LocationAlert/LocationAlert.Library/Models/Message.cs
+++ LocationAlert/LocationAlert.Library/Models/Message.cs
@@ -29,13 +29,37 @@ namespace LocationAlert.Library.Models
             {
                 regString += ("Update for Region" + region.name + "/n");
 
+                //****Change 1 to severity enum or switch string case based on severity int
+                if (preferences.alwaysTemp || (CheckRange(1, preferences.temp[0], preferences.temp[1])))
+                {
+                    regString += ("Temperature: " + 1 + "/n");
+                }
+
                 if (preferences.alwaysRain || (CheckRange(1,preferences.rain[0],preferences.rain[1])))
                 {
-                    regString += ("Rain Severity: " + 1);
+                    regString += ("Rain Severity: " + 1 + "/n");
+                }
+
+                if (preferences.alwaysSnow || (CheckRange(1, preferences.snow[0], preferences.snow[1])))
+                {
+                    regString += ("Snow Severity: " + 1 + "/n");
+                }
+
+                if (preferences.alwaysWind || (CheckRange(1, preferences.wind[0], preferences.wind[1])))
+                {
+                    regString += ("Wind Severity: " + 1 + "/n");
+                }
+
+                if (preferences.alwaysCloud || (CheckRange(1, preferences.cloud[0], preferences.cloud[1])))
+                {
+                    regString += ("Cloud Severity: " + 1 + "/n");
+                }
+
+                if (preferences.alwaysHumidity || (CheckRange(1, preferences.humidity[0], preferences.humidity[1])))
+                {
+                    regString += ("Humidity Severity: " + 1 + "/n");
                 }
             }
-            
-            
         }
 
         private bool CheckRange(int value, int min, int max)
