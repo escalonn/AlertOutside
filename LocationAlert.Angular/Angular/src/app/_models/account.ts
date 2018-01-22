79fb7ed2f61c35c9f70085c7b38adc1d33b957fd
sofani <sofanib@gmail.com>
2018-01-22 14:49:19 -0500

    updated html and css

--- LocationAlert.Angular/Angular/src/app/_models/account.ts
+++ LocationAlert.Angular/Angular/src/app/_models/account.ts
@@ -2,7 +2,7 @@ import { Region } from "./region";
 import { WeatherPreference } from "./weatherpreference";
 
 export class Account {
-  email: string = "Enter Email Address";
+  email: string = "";
   regions: Region[] = [];
   weatherPref: WeatherPreference = new WeatherPreference();
 }
