d001ee1f1159e0023fb44ad1d017597f1a5a65f3
Nicholas Escalona <nicholas.escalona@gmail.com>
2018-01-25 21:15:00 -0500

    dockerize using nick docker hub repos.

--- LocationAlert.Angular/Angular/src/environments/environment.prod.ts
+++ LocationAlert.Angular/Angular/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  libraryServiceUri: "http://ec2-34-201-125-246.compute-1.amazonaws.com/LocationAlertLibrary"
+  // libraryServiceUri: "http://ec2-34-201-125-246.compute-1.amazonaws.com/LocationAlertLibrary"
+  libraryServiceUri: "http://localhost:20000"
 };
